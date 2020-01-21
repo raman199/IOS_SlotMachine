6db76aa5b045175c890d750a1e568fca931acf53
Ramandeep Kaur <pawarraman9@gmail.com>
2020-01-20 20:41:15 -0500

    Initial Commit

---
+++ SlotMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlotMachine
+//
+//  Created by mac on 2020-01-20.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
